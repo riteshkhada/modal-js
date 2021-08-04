0c441c9af317216e7482cdf8fa44329431026fd0
riteshkhadka <khadkaritesh45@gmail.com>
2021-08-04 12:18:03 +0545

    new commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const showModal = document.querySelectorAll(".show-modal");
+const closeModal = document.querySelector(".close-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const btnCloseModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < showModal.length; i++)
+  showModal[i].addEventListener("click", openModal);
+
+closeModal.addEventListener("click", btnCloseModal);
+overlay.addEventListener("click", btnCloseModal);
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+
+  if (e.key === "Escape") {
+    if (!modal.classList.contains("hidden")) {
+      btnCloseModal();
+    }
+  }
+});
